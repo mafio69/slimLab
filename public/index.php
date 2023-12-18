8707e2b3ab0ba1a6f9af74d09a56239f872df46d
mariusz <mariusz.franciszczak@rubix.com>
2023-12-18 19:16:27 +0100

    Add routes.php

--- public/index.php
+++ public/index.php
@@ -2,27 +2,9 @@
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
-use Psr\Http\Message\RequestInterface;
-use Psr\Http\Message\ResponseInterface;
 use Slim\Factory\AppFactory;
 
 $app = AppFactory::create();
 
-$app->get('/', function (RequestInterface $request, ResponseInterface $response, array $args) {
-    $response->getBody()->write('Hello from Slim 4 request handler');
-    return $response;
-});
-
-$app->map(['GET', 'POST'], '/books', function ($request, $response, array $args) {
-    $response->getBody()->write(sprintf("Hello, %s!", 'mafio'));
-
-    return $response;
-});
-
-$app->get('/hello/{name}', function (RequestInterface $request, ResponseInterface $response, array $args) {
-    $response->getBody()->write('Hello from Slim 4 request handler ' . $args['name']);
-
-    return $response;
-});
-
+(require __DIR__ . '/../src/route/routes.php')($app);
 $app->run();
---
+++ src/route/routes.php
@@ -0,0 +1,22 @@
+<?php
+
+use Slim\App;
+
+return function (App $app) {
+    $app->get('/', function (RequestInterface $request, ResponseInterface $response, array $args) {
+        $response->getBody()->write('Hello from Slim 4 request handler');
+        return $response;
+    });
+
+    $app->map(['GET', 'POST'], '/books', function ($request, $response, array $args) {
+        $response->getBody()->write(sprintf("Hello, %s!", 'mafio'));
+
+        return $response;
+    });
+
+    $app->get('/hello/{name}', function (RequestInterface $request, ResponseInterface $response, array $args) {
+        $response->getBody()->write('Hello from Slim 4 request handler ' . $args['name']);
+
+        return $response;
+    });
+};
